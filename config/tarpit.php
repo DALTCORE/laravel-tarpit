aed5f8f0b03ab40460003887a8349f92af400ec3
RamonSmit <ramon@daltcore.com>
2017-06-02 15:48:15 +0000

    Apply fixes from StyleCI

--- config/tarpit.php
+++ config/tarpit.php
@@ -2,33 +2,33 @@
 
 return [
 
-    /**
+    /*
      * Enable the HTTP-Tarpit
      */
     'enabled' => env('TARPIT_ENABLE', true),
 
-    /**
+    /*
      * HTTP-Tarpit API Key
      */
     'key'     => env('TARPIT_API_KEY', false),
 
-    /**
+    /*
      * API URL
      */
     'url'     => env('TARPIT_ENDPOINT', 'api.http-tarpit.org'),
 
-    /**
+    /*
      * API Version
      */
     'version' => env('TARPIT_VERSION', 'v2'),
 
-    /**
+    /*
      * Handle requests in realtime or via cache
      */
     'type'    => env('TARPIT_HANDLER', 'realtime'),
 
-    /**
+    /*
      * Current website domain
      */
-    'domain'  => env('TARPIT_DOMAIN', env('APP_URL', 'unknown.com'))
+    'domain'  => env('TARPIT_DOMAIN', env('APP_URL', 'unknown.com')),
 ];
